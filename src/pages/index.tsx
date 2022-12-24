54d28d82a94bdfe6accddce1d9a3c724822cfcc3
nathanjessen <nathan.jessen@gmail.com>
2022-12-24 16:09:39 -0600

    update app setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Better AI</title>
+        <meta name='description' content='AI tools to improve your workflow' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      </main>
+    </>
+  );
+}
